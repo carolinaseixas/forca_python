259ef97ec6ec2ec5c8e702f0e63c0586714c795d
Carolina Seixas <91434852+carolinaseixas@users.noreply.github.com>
2022-01-11 16:00:43 -0300

    Adicionando explicações ao código

--- forca.py
+++ forca.py
@@ -2,32 +2,36 @@ import re
 
 def escolhe_letra():
     escolha = input("Por favor, escolha uma letra de A a Z sem acentos: ")
+    #Checando se a entrada do usuário é uma única letra
     while (re.search('[^a-zA-Z]', escolha)) or (len(escolha) != 1):
         escolha = input("Escolha inválida. Por favor, escolha uma letra de A a Z sem acentos: ")
     return escolha
 
 
-
+#Deve-se definir a palavra aqui
 palavra = 'abracadabra'
 
+#Imprimindo a abertura do jogo e um _ para cada letra da palavra
 print("***JOGO DA FORCA***\n")
 print("Esta é sua palavra: \n")
-print("_ " * len(palavra), end="")
-print("\n")
+print("_ " * len(palavra))
 
-
+#Inicializando as variáveis de tentativas, de letras descobertas e de letras restantes para descobrir
 tentativas_restantes = 6
 letras_restantes = len(palavra)
 palavra_atual = list("_" * len(palavra))
 
+#Enquanto ainda houver tentativas e letras para descobrir
 while (tentativas_restantes > 0) and (letras_restantes > 0):
     letra_escolhida = escolhe_letra()
     letra_escolhida = letra_escolhida.lower()
 
+    #Se a letra escolhida não pertence à palavra, subtrair uma tentativa
     if letra_escolhida not in palavra:
         tentativas_restantes -= 1
         print('\nA letra escolhida não faz parte da palavra. Você perdeu uma tentativa.\n')
 
+    #Se a letra escolhida estiver na palavra, diminuir a quantidade de letras que faltam encontrar e preencher a palavra exibida para o usuário com ela
     index = 0
     for letra in palavra:
         if letra == letra_escolhida:
@@ -35,6 +39,7 @@ while (tentativas_restantes > 0) and (letras_restantes > 0):
             palavra_atual[index] = letra_escolhida
         index += 1
 
+    #Exibir a palavra para o usuário com as letras já descobertas
     for letra in palavra_atual:
         print(letra, "", end="")
 
