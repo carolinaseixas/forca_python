18cc28ffa6b4bf8ad2dec9b73ba14e6e3632d75e
Carolina Seixas <carolxleite@gmail.com>
2023-11-25 13:57:35 -0300

    melhorando o for e quebras de linha

--- forca.py
+++ forca.py
@@ -3,7 +3,7 @@ import re
 
 def sortear_palavra():
     palavras_possiveis = ['abacadabra', 'finalidade', 'idoneidade', 'sublime', 'cultura', 'alcunha']
-    posicao_da_palavra = random.randint(0, len(palavras_possiveis))
+    posicao_da_palavra = random.randint(0, len(palavras_possiveis)-1)
     return palavras_possiveis[posicao_da_palavra]
 
 def escolher_letra():
@@ -23,8 +23,7 @@ palavra_descoberta = list("_" * len(palavra_sorteada))
 
 #Enquanto ainda houver tentativas e letras para descobrir
 while (tentativas_restantes > 0) and (letras_restantes > 0):
-    letra_escolhida = escolher_letra()
-    letra_escolhida = letra_escolhida.lower()
+    letra_escolhida = escolher_letra().lower()
 
     #Se a letra escolhida não pertence à palavra, subtrair uma tentativa
     if letra_escolhida not in palavra_sorteada:
@@ -32,25 +31,24 @@ while (tentativas_restantes > 0) and (letras_restantes > 0):
         print('\nA letra escolhida não faz parte da palavra. Você perdeu uma tentativa.\n')
 
     #Se a letra escolhida estiver na palavra, diminuir a quantidade de letras que faltam encontrar e preencher a palavra exibida para o usuário com ela
-    index = 0
-    for letra in palavra_sorteada:
+    for posicao, letra in enumerate(palavra_sorteada):
         if letra == letra_escolhida:
             letras_restantes -= 1
-            palavra_descoberta[index] = letra_escolhida
-        index += 1
+            palavra_descoberta[posicao] = letra_escolhida
 
     #Exibir a palavra para o usuário com as letras já descobertas
     for letra in palavra_descoberta:
         print(letra, "", end="")
 
 
-    print("\nVocê possui mais {} tentativa(s).\n".format(tentativas_restantes))
+    if letras_restantes > 0:
+        print("\nVocê possui mais {} tentativa(s).\n".format(tentativas_restantes))
 
 
 if tentativas_restantes == 0:
-    print('Acabaram as tentativas :(\nVocê foi enforcado!')
+    print('\nAcabaram as tentativas :(\nVocê foi enforcado!')
 
 if letras_restantes == 0:
-    print('Parabéns! Você descobriu que a palavra secreta é {}!'.format(palavra_sorteada))
+    print('\nParabéns! Você descobriu que a palavra secreta é {}!'.format(palavra_sorteada))
 
 print("\nFim do jogo")
