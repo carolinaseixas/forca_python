75dc92937c1a36c83f38f3ec0b64ac1565ff5b19
Carolina Seixas <carolxleite@gmail.com>
2023-11-25 13:22:21 -0300

    trocando palavra unica por sorteio de palavra

--- forca.py
+++ forca.py
@@ -1,46 +1,46 @@
+import random
 import re
 
-def escolhe_letra():
+def sortear_palavra():
+    palavras_possiveis = ['abacadabra', 'finalidade', 'idoneidade', 'sublime', 'cultura', 'alcunha']
+    posicao_da_palavra = random.randint(0, len(palavras_possiveis))
+    return palavras_possiveis[posicao_da_palavra]
+
+def escolher_letra():
     escolha = input("Por favor, escolha uma letra de A a Z sem acentos: ")
-    #Checando se a entrada do usuário é uma única letra
     while (re.search('[^a-zA-Z]', escolha)) or (len(escolha) != 1):
         escolha = input("Escolha inválida. Por favor, escolha uma letra de A a Z sem acentos: ")
     return escolha
 
-
-#Deve-se definir a palavra aqui
-palavra = 'abracadabra'
-
-#Imprimindo a abertura do jogo e um _ para cada letra da palavra
 print("***JOGO DA FORCA***\n")
 print("Esta é sua palavra: \n")
-print("_ " * len(palavra))
+palavra_sorteada = sortear_palavra()
+print("_ " * len(palavra_sorteada))
 
-#Inicializando as variáveis de tentativas, de letras descobertas e de letras restantes para descobrir
 tentativas_restantes = 6
-letras_restantes = len(palavra)
-palavra_atual = list("_" * len(palavra))
+letras_restantes = len(palavra_sorteada)
+palavra_descoberta = list("_" * len(palavra_sorteada))
 
 #Enquanto ainda houver tentativas e letras para descobrir
 while (tentativas_restantes > 0) and (letras_restantes > 0):
-    letra_escolhida = escolhe_letra()
+    letra_escolhida = escolher_letra()
     letra_escolhida = letra_escolhida.lower()
 
     #Se a letra escolhida não pertence à palavra, subtrair uma tentativa
-    if letra_escolhida not in palavra:
+    if letra_escolhida not in palavra_sorteada:
         tentativas_restantes -= 1
         print('\nA letra escolhida não faz parte da palavra. Você perdeu uma tentativa.\n')
 
     #Se a letra escolhida estiver na palavra, diminuir a quantidade de letras que faltam encontrar e preencher a palavra exibida para o usuário com ela
     index = 0
-    for letra in palavra:
+    for letra in palavra_sorteada:
         if letra == letra_escolhida:
             letras_restantes -= 1
-            palavra_atual[index] = letra_escolhida
+            palavra_descoberta[index] = letra_escolhida
         index += 1
 
     #Exibir a palavra para o usuário com as letras já descobertas
-    for letra in palavra_atual:
+    for letra in palavra_descoberta:
         print(letra, "", end="")
 
 
@@ -51,6 +51,6 @@ if tentativas_restantes == 0:
     print('Acabaram as tentativas :(\nVocê foi enforcado!')
 
 if letras_restantes == 0:
-    print('Parabéns! Você descobriu que a palavra secreta é {}!'.format(palavra))
+    print('Parabéns! Você descobriu que a palavra secreta é {}!'.format(palavra_sorteada))
 
 print("\nFim do jogo")
